928c208b42a99d95ce8b8d41e55c58dafe027a24
bmaupin <bmaupin@users.noreply.github.com>
2021-11-10 11:55:22 -0500

    feat: Add EST workaround
    And make the purpose of the UTC workarounds more clear

--- timezone.test.js
+++ timezone.test.js
@@ -1,15 +1,3 @@
-test('Test explicit UTC dates', () => {
-  // This will always be 2028-11-05T00:00:00.000Z regardless of the timezone
-  expect(new Date('2028-11-05T00:00:00.000Z').toISOString()).toEqual(
-    '2028-11-05T00:00:00.000Z'
-  );
-
-  // Another way of doing the same thing
-  expect(
-    new Date(new Date(2028, 10, 5).setUTCHours(0, 0, 0, 0)).toISOString()
-  ).toEqual('2028-11-05T00:00:00.000Z');
-});
-
 test('Test TZ=EST', () => {
   // This is always be UTC - 5, regardless of daylight saving time
   process.env.TZ = 'EST';
@@ -18,6 +6,13 @@ test('Test TZ=EST', () => {
   // This _should_ create a date with his day and a time of midnight EST (as set in process.env.TZ above)
   // Instead, it appears to use the value of TZ that's set before node is run, so it fails unless TZ is set to EST before node is run, e.g. `TZ=EST npm test`
   expect(new Date(2028, 10, 5).toISOString()).toEqual(
+    '2028-11-05T05:00:00.000Z'
+  );
+});
+
+test('Test TZ=EST workaround', () => {
+  // This works, but I don't know how one would handle daylight saving time this way; probably best to stick with UTC
+  expect(new Date('2028-11-05T00:00:00.000-05:00').toISOString()).toEqual(
     '2028-11-05T05:00:00.000Z'
   );
 });
@@ -31,4 +26,16 @@ test('Test TZ=UTC', () => {
   expect(new Date(2028, 10, 5).toISOString()).toEqual(
     '2028-11-05T00:00:00.000Z'
   );
+});
+
+test('Test TZ=UTC workarounds', () => {
+  // This will always be 2028-11-05T00:00:00.000Z regardless of the timezone
+  expect(new Date('2028-11-05T00:00:00.000Z').toISOString()).toEqual(
+    '2028-11-05T00:00:00.000Z'
+  );
+
+  // Another way of doing the same thing
+  expect(
+    new Date(new Date(2028, 10, 5).setUTCHours(0, 0, 0, 0)).toISOString()
+  ).toEqual('2028-11-05T00:00:00.000Z');
 });
